bd6481dcab26111003502fc69f377d3728684fac
ChiaoChiehChen <qa6248@gmail.com>
2021-12-17 17:57:17 +0800

    修改使用介紹內容

--- introduce.js
+++ introduce.js
@@ -3,7 +3,7 @@ export default (event) => {
     if (event.message.text === '使用介紹') {
       event.reply({
         type: 'text',
-        text: '☕How to use?☕\n 輸入\n 1️⃣ !name 咖啡廳店名➔ 即可獲得店家位置及資訊，e.g. !name 蜂巢咖啡\n\n 2️⃣ 不限時➔ 即可獲得隨機8家沒有限時用餐的咖啡廳 \n\n 3️⃣ 傳送 📍目前位置所在地➔ 即可獲得鄰近10公里處的5家咖啡廳。'
+        text: '☕How to use?☕\n 輸入\n 1️⃣ !name 咖啡廳店名➔ 即可獲得店家位置及資訊，e.g. !name 蜂巢咖啡\n\n 2️⃣ 不限時➔ 即可獲得隨機8家沒有限時用餐的咖啡廳 \n\n 3️⃣ 傳送 📍目前位置所在地➔ 即可獲得鄰近5公里處的5家咖啡廳。'
       })
     }
   } catch (error) {
